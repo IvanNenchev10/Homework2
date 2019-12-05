05f72848f9d273bc66035de553caed0d33de04be
IvanNenchev10 <56735762+IvanNenchev10@users.noreply.github.com>
2019-12-05 11:28:43 +0200

    Add files via upload

---
+++ Homework2.10.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Задача10
+{
+    class Program
+    {
+        static void Main()
+        {
+            int number, sum = 0, counter = 0;
+            while(counter<4)
+            {
+                Console.WriteLine("Enter number:");
+                number = int.Parse(Console.ReadLine());
+                sum = sum + number;
+                counter++;
+            }
+            Console.WriteLine("Sum={0}", sum);
+        }
+    }
+}
