a03963762466a5fa23f5309f07897e0c3ff53269
IvanNenchev10 <56735762+IvanNenchev10@users.noreply.github.com>
2019-12-05 11:29:08 +0200

    Add files via upload

---
+++ Homework2.11.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Задача11
+{
+    class Program
+    {
+        static void Main()
+        {
+            int radius;
+            double area, perimeter;
+            Console.WriteLine("Enter radius of the circle:");
+            radius = int.Parse(Console.ReadLine());
+            area = Math.PI * Math.Pow(radius, 2);
+            Console.WriteLine("Area of the circle is:{0}", Math.Round(area,2));
+            perimeter = 2 * (Math.PI * radius);
+            Console.WriteLine("Perimeter of the rectangle is:{0}", Math.Round(perimeter,2));
+
+        }
+        
+    }
+}
