0540d2fc18185055c08a4aed4745ddbc931572a5
IvanNenchev10 <56735762+IvanNenchev10@users.noreply.github.com>
2019-12-05 11:27:34 +0200

    Add files via upload

---
+++ Homework2.7.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Задача7
+{
+    class Program
+    {
+        static void Main()
+        {
+            float height, width;
+            decimal result = 0.0m;
+            Console.WriteLine("Enter height of the triangle:");
+            height = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter width of the triangle:");
+            width = float.Parse(Console.ReadLine());
+            result = (decimal)height * (decimal)width;
+            Console.WriteLine("Area of the rectangle is:{0}", Math.Round(result, 2));
+        }
+    }
+}
