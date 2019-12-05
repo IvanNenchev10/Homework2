975b93310d577e0cf0cb3852272cdc144c9703ad
IvanNenchev10 <56735762+IvanNenchev10@users.noreply.github.com>
2019-12-05 11:25:18 +0200

    Add files via upload

---
+++ Homework2.3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Задача3
+{
+    class Program
+    {
+        static void Main()
+        {
+            string name=" ", gender, colour;
+            int age;
+            double sum;
+            char ch;
+            Console.WriteLine("Enter name:");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter gender:");
+            gender = Console.ReadLine();
+            Console.WriteLine("Enter colour of the eyes");
+            colour = Console.ReadLine();
+            Console.WriteLine("Enter age:");
+            age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter sum:");
+            sum = double.Parse(Console.ReadLine());
+            ch = name[0];
+            Console.WriteLine("{0}", name);
+            Console.WriteLine("{0}", age);
+            Console.WriteLine("{0}", gender);
+            Console.WriteLine("{0}", sum);
+            Console.WriteLine("{0}", colour);
+            Console.WriteLine("First index of name is:{0}", ch);
+        }
+    }
+}
