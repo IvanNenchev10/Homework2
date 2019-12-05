4beb9f6b0c2e4ac6be0bfc84ab0655d2cfb53df4
IvanNenchev10 <56735762+IvanNenchev10@users.noreply.github.com>
2019-12-05 11:26:27 +0200

    Add files via upload

---
+++ Homework2.6.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace Задача6
+{
+    class Program
+    {
+        static void Main()
+        {
+            string A, B, C;
+            Console.WriteLine("Enter first string:");
+            A = Console.ReadLine();
+            Console.WriteLine("Enter second string:");
+            B = Console.ReadLine();
+            C = A + " " + B;
+            Console.WriteLine("String after concatenation is:{0}", C);
+        }
+    }
+}
